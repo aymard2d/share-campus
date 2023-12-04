07ff4d91619a95b3ab64c65111092cd5ff31a5ca
Sarah Andre <sarahandrre@gmail.com>
2023-12-04 19:10:18 +0100

    index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,25 @@
 class CoursesController < ApplicationController
   def index
-  end
-
-  def new
+    @courses = Course.all
     @course = Course.new
+    @user = current_user
   end
 
   def create
+    @course = Course.new(course_params)
+    @user = current_user
+    @course.user_id = current_user
+    if @course.save
+      redirect_to courses_path, notice: "Cours enregistré"
+    else
+      render "courses/index", status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :summarize, :category, :date, :user_id)
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,3 @@
 class Course < ApplicationRecord
   belongs_to :user
-  has_many
 end
