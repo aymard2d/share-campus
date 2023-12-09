502634471cc3ee8004c7eec8e94d942c986121aa
Sarah Andre <sarahandrre@gmail.com>
2023-12-10 00:57:09 +0100

    user card

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable
+  devise :recoverable, :rememberable, :validatable
 
   has_many :courses
   has_many :messages, dependent: :destroy
