cac0e7eaaced13b7c918d28abbb457692b7fae96
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2023-12-12 10:39:15 +0100

    ok

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   has_one_attached :document
 
   validates :title, presence: true
+  validates :document, presence: true
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes, source: :courses
 
