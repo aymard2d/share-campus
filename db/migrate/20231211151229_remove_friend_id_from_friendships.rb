68988f503e835c00648e72ac4f87b37ab6b64b4a
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-03-05 13:48:30 +0100

    ok

--- db/migrate/20231211151229_remove_friend_id_from_friendships.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFriendIdFromFriendships < ActiveRecord::Migration[7.1]
-  def change
-    remove_reference :friendships, :friend, foreign_key: true
-  end
-end
--- db/migrate/20231211151426_remove_friends.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFriends < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :friends
-  end
-end
